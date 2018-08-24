753c171ebc770d2a16b1f9dcccc2a79d37eecff8
porkunanongithub <41177840+porkunanongithub@users.noreply.github.com>
2018-08-24 16:27:54 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'id6730027_akirastore');
+define('DB_NAME', 'akirastoreshop');
 
 /** MySQL database username */
-define('DB_USER', 'id6730027_akirastore');
+define('DB_USER', 'akirastoreshop');
 
 /** MySQL database password */
 define('DB_PASSWORD', '0843213453');
 
 /** MySQL hostname */
-define('DB_HOST', 'files.000webhost.com');
+define('DB_HOST', 'db4free.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
